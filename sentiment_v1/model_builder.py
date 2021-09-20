2a1f02af79a39c014164fdd05e5236b2aaded397
Sreram Kasiviswanathan <sreramk26@gmail.com>
2021-09-20 17:23:13 +0530

    Fixed a bug in SentimentLSTMDense64Dense32Dense16Dense8Dense1
    The model builder must also accept weights.

--- sentiment_v1/model_builder.py
+++ sentiment_v1/model_builder.py
@@ -19,14 +19,18 @@ import numpy as np
 from train_validate_predict.model_builder_base import ModelBuilderBase
 from train_validate_predict.dataset_base import DataSetBase
 
+TfDsStoreMaxTokensAndLayeredWeightsAreNoneError = Exception("tf_ds_store, layered_weights and max_tokens are None")
+
 
 class SentimentLSTMDense64Dense32Dense16Dense8Dense1(ModelBuilderBase):
 
     def __init__(self,
                  tf_ds_store: DataSetBase,
-                 max_tokens=None):
-        self.__max_tokens = max_tokens
+                 max_tokens=None,
+                 layered_weights=None):
         self.__tf_ds_store = tf_ds_store
+        self.__max_tokens = max_tokens
+        self.__layered_weights = layered_weights
         self.__encoder = None
         self.__model = None
 
@@ -36,18 +40,38 @@ class SentimentLSTMDense64Dense32Dense16Dense8Dense1(ModelBuilderBase):
     def __create_encoder(self):
         self.__encoder = tf.keras.layers.TextVectorization(max_tokens=self.__max_tokens)
 
-        if self.__tf_ds_store is None:
-            self.__encoder.adapt(np.array([['placeholder']], dtype=np.object), batch_size=None)
+        if self.__layered_weights is None and self.__tf_ds_store is None and self.__layered_weights is None:
+            raise TfDsStoreMaxTokensAndLayeredWeightsAreNoneError
+
+        if self.__layered_weights is None:
+            if self.__tf_ds_store is None:
+                self.__encoder.adapt(np.array([['placeholder']], dtype=np.object), batch_size=None)
+            else:
+                self.__encoder.adapt(self.__tf_ds_store.prepare_and_get_train_ds().map(lambda text, label: text))
         else:
-            self.__encoder.adapt(self.__tf_ds_store.prepare_and_get_train_ds().map(lambda text, label: text))
+            self.__encoder.adapt(np.array([['placeholder']], dtype=np.object), batch_size=None)
+            self.__encoder.set_weights(self.__layered_weights[0])
 
         return self.__encoder
+
+    def __resolve_embedding_vocabulary_size(self, encoder):
+        if self.__tf_ds_store is None:
+            if self.__max_tokens is not None:
+                embedding_vocabulary_size = self.__max_tokens
+            elif self.__layered_weights is not None:
+                embedding_vocabulary_size = len(encoder.get_vocabulary())
+            else:
+                raise TfDsStoreMaxTokensAndLayeredWeightsAreNoneError
+        else:
+            embedding_vocabulary_size = len(encoder.get_vocabulary())
+        return embedding_vocabulary_size
 
     def build_model(self):
         if self.__encoder is None:
             encoder = self.__create_encoder()
 
-            embedding_vocabulary_size = len(encoder.get_vocabulary())
+            embedding_vocabulary_size = self.__resolve_embedding_vocabulary_size(encoder)
+
             embedding_output_dimensions = 64
             mask_zero = True
 
@@ -74,6 +98,14 @@ class SentimentLSTMDense64Dense32Dense16Dense8Dense1(ModelBuilderBase):
                 tf.keras.layers.Dense(dense_layer_4_units, activation=dense_activation),
                 tf.keras.layers.Dense(dense_layer_5_output_layer_units)  # , activation='sigmoid')
             ])
+
+            # Add the weights to the model, if it was given
+            if self.__layered_weights is not None:
+                i = 0
+                for layer in self.__model.layers:
+                    layer.set_weights(self.__layered_weights[i])
+                    i += 1
+
             return True
 
         return False
